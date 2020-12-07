04e12b910e811e6e440c9744ea01e530a739f63e
nicholas-kotsakis <nkotsakis13@gmail.com>
2020-12-06 19:29:23 -0500

    Prints CSV file contents

--- src/main/java/com/worklogix/falcon/dao/DataImport.java
+++ src/main/java/com/worklogix/falcon/dao/DataImport.java
@@ -1,9 +1,18 @@
 package com.worklogix.falcon.dao;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DataImport {
 
-    public void importData(String filename) {
+    public void importData(String fileName) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader(fileName));
+        String line;
 
+        while ((line = input.readLine()) != null){
+            System.out.println(line);
+        }
     }
 
 }
--- src/main/java/com/worklogix/falcon/service/UploadService.java
+++ src/main/java/com/worklogix/falcon/service/UploadService.java
@@ -1,22 +1,26 @@
 package com.worklogix.falcon.service;
 
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import com.worklogix.falcon.dao.DataImport;
+
 
 public class UploadService implements IUploadService {
 
     @Override
     public void saveDataFile(MultipartFile dataFile) throws IOException {
         String folder = System.getProperty("user.home") + "/Downloads/";
+        DataImport dataImport = new DataImport();
 
         byte[] bytes = dataFile.getBytes();
         Path path = Paths.get(folder + dataFile.getOriginalFilename());
         //Write the file locally so we can read it into a database
         Files.write(path, bytes);
+        dataImport.importData(folder.concat(dataFile.getOriginalFilename()));
+
 
     }
 }
